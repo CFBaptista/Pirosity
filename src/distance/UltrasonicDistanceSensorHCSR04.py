5c890f1c1b4d7a2c66192c45065deee60629ec2e
Carlos Fernando Baptista <cfd.baptista@gmail.com>
2025-01-24 15:52:22 +0100

    Add class

---
+++ src/distance/UltrasonicDistanceSensorHCSR04.py
@@ -0,0 +1,68 @@
+from dataclasses import dataclass
+import time
+
+import RPi.GPIO as GPIO
+
+
+@dataclass
+class HCSR04Data:
+    duration_timeout: float = 0.038
+    duration_trigger: float = 0.00001
+    duration_wait: float = 0.000002
+
+
+class UltrasonicDistanceSensorHCSR04:
+    def __init__(self, pin_trigger: int, pin_echo: int, speed_of_sound: float = 343.0) -> None:
+        self._pin_trigger = pin_trigger
+        self._pin_echo = pin_echo
+        self._speed_of_sound = speed_of_sound
+
+        self._configure_gpio()
+
+    def _configure_gpio(self) -> None:
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setup(self._pin_trigger, GPIO.OUT)
+        GPIO.setup(self._pin_echo, GPIO.IN)
+
+    def measure(self) -> float:
+        self._trigger_burst()
+        transmit_end_time = self._transmit_end_time()
+        receive_start_time = self._receive_start_time()
+        distance = self._compute_distance(transmit_end_time, receive_start_time)
+        return distance
+
+    def _trigger_burst(self) -> None:
+        GPIO.output(self._trigger_pin, False)
+        time.sleep(HCSR04Data.duration_wait)
+        GPIO.output(self._trigger_pin, True)
+        time.sleep(HCSR04Data.duration_trigger)
+        GPIO.output(self._trigger_pin, False)
+
+    def _transmit_end_time(self) -> float:
+        while GPIO.input(self._echo_pin) is False:
+            pass
+        transmitted_signal_end_time = time.time()
+        return transmitted_signal_end_time
+
+    def _receive_start_time(self) -> float:
+        while GPIO.input(self._echo_pin) is True:
+            pass
+        received_signal_start_time = time.time()
+        return received_signal_start_time
+
+    def _compute_distance(self, transmit_end_time: float, receive_start_time: float) -> float:
+        time_elapsed = receive_start_time - transmit_end_time
+        distance = HCSR04Data.speed_of_sound * time_elapsed / 2
+        return distance
+
+    @property
+    def pin_trigger(self) -> int:
+        return self._pin_trigger
+
+    @property
+    def pin_echo(self) -> int:
+        return self._pin_echo
+
+    @property
+    def speed_of_sound(self) -> float:
+        return self._speed_of_sound
