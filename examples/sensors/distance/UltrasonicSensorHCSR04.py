bd294603d1de59f19372c5d52ef4dfa598e0e728
Carlos Fernando Baptista <cfd.baptista@gmail.com>
2025-01-27 17:34:40 +0100

    Update example

--- examples/sensors/distance/UltrasonicSensorHCSR04.py
+++ examples/sensors/distance/UltrasonicSensorHCSR04.py
@@ -2,17 +2,16 @@ import time
 
 from pirosity.sensors.distance import UltrasonicSensorHCSR04
 
-sensor = UltrasonicSensorHCSR04(trigger_pin=16, echo_pin=18, speed_of_sound=343.0)
+sensor = UltrasonicSensorHCSR04(16, 18, speed_of_sound=343.0)
 
-while True:
-    try:
+try:
+    while True:
         distance = sensor.measure()
         print(f"Distance: {distance * 100} cm")
         time.sleep(0.25)
-    except KeyboardInterrupt:
-        print("Exiting program due to keyboard interruption.")
-    except Exception as exception:
-        print(f"An error occurred: {exception}")
-    finally:
-        sensor.reset()
-        break
+except KeyboardInterrupt:
+    print("Exiting program due to keyboard interruption.")
+except Exception as exception:
+    print(f"An error occurred: {exception}")
+finally:
+    sensor.reset()
