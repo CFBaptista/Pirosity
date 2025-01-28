ca8c51fdb5b018970fa3dbfb0c13c93cdecefc31
Carlos Fernando Baptista <cfd.baptista@gmail.com>
2025-01-28 02:03:07 +0100

    Add gpiozero-based HC-SR04

--- pirosity/sensors/distance/__init__.py
+++ pirosity/sensors/distance/__init__.py
@@ -1,3 +1,4 @@
 from .UltrasonicSensorHCSR04 import UltrasonicSensorHCSR04
+from .UltrasonicSensorHCSR04_5 import UltrasonicSensorHCSR04_5
 
-__all__ = ["UltrasonicSensorHCSR04"]
+__all__ = ["UltrasonicSensorHCSR04", "UltrasonicSensorHCSR04_5"]
