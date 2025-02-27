6b04994ab3e5e144dff00eb85280161c8cdb7b54
Carlos Fernando Baptista <cfd.baptista@gmail.com>
2025-02-27 11:48:09 +0100

    Fix import

--- pirosity/core/sensors/__init__.py
+++ pirosity/core/sensors/__init__.py
@@ -1,3 +1,3 @@
-from .distance import UltrasonicSensorHCSR04, UltrasonicSensorHCSR04Data
+from .distance.UltrasonicSensorHCSR04 import UltrasonicSensorHCSR04, UltrasonicSensorHCSR04Data
 
 __all__ = ["UltrasonicSensorHCSR04", "UltrasonicSensorHCSR04Data"]
