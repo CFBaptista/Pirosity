31ad9c18eaa3fb395a45f917ade03be30893e97c
Carlos Fernando Baptista <cfd.baptista@gmail.com>
2025-02-27 09:31:58 +0100

    Format file

--- pirosity/ros/src/hcsr04/setup.py
+++ pirosity/ros/src/hcsr04/setup.py
@@ -1,26 +1,23 @@
 from setuptools import find_packages, setup
 
-package_name = 'hcsr04'
+package_name = "hcsr04"
 
 setup(
     name=package_name,
-    version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    version="0.0.0",
+    packages=find_packages(exclude=["test"]),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
     ],
-    install_requires=['setuptools'],
+    install_requires=["setuptools"],
     zip_safe=True,
-    maintainer='Carlos Fernando Baptista',
-    maintainer_email='cfd.baptista@gmail.com',
-    description='A ROS2 pacakge for interfacing with the HC-SR04 ultrasonic sensor',
-    license='MIT',
-    tests_require=['pytest'],
+    maintainer="Carlos Fernando Baptista",
+    maintainer_email="cfd.baptista@gmail.com",
+    description="A ROS2 pacakge for interfacing with the HC-SR04 ultrasonic sensor",
+    license="MIT",
+    tests_require=["pytest"],
     entry_points={
-        'console_scripts': [
-            'ultrasonic_distance = hcsr04.ultrasonic_distance:main'
-        ],
+        "console_scripts": ["ultrasonic_distance = hcsr04.ultrasonic_distance:main"],
     },
 )
