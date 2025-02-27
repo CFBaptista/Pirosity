c216487cb8011a97ac0453e94bd25ea53fafd8b1
Carlos Fernando Baptista <cfd.baptista@gmail.com>
2025-02-27 16:15:41 +0100

    Add publisher node for HC-SR04 sensor

--- pirosity/ros/src/hcsr04/hcsr04/ultrasonic_distance.py
+++ pirosity/ros/src/hcsr04/hcsr04/ultrasonic_distance.py
@@ -1,6 +1,35 @@
-def main():
-    print('Hi from hcsr04.')
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import Float64
+
+from pirosity.core.sensors import UltrasonicSensorHCSR04
 
 
-if __name__ == '__main__':
+class UltrasonicDistancePublisher(Node):
+    def __init__(self):
+        self.sensor = UltrasonicSensorHCSR04(23, 24)
+
+        super().__init__("ultrasonic_distance_publisher")
+        self.publisher_ = self.create_publisher(Float64, "ultrasonic_distance", 10)
+        self.timer = self.create_timer(0.5, self.timer_callback)
+
+    def timer_callback(self) -> None:
+        distance_message = Float64()
+        distance_message.data = self.sensor.measure()
+
+        self.publisher_.publish(distance_message)
+        self.get_logger().info(f"Publishing measured distance in meters: {distance_message.data}")
+
+
+def main(args=None) -> None:
+    rclpy.init(args=args)
+    ultrasonic_distance = UltrasonicDistancePublisher()
+
+    rclpy.spin(ultrasonic_distance)
+
+    ultrasonic_distance.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == "__main__":
     main()
--- pirosity/ros/src/hcsr04/setup.py
+++ pirosity/ros/src/hcsr04/setup.py
@@ -1,20 +1,26 @@
+import os
+
 from setuptools import find_packages, setup
 
 package_name = "hcsr04"
+requirements_filepath = os.path.join(os.path.dirname(__file__), "requirements.txt")
+
+with open(requirements_filepath) as requirements_file:
+    requirements = requirements_file.read().splitlines()
 
 setup(
     name=package_name,
-    version="0.0.0",
+    version="0.1.0.dev0",
     packages=find_packages(exclude=["test"]),
     data_files=[
         ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
         ("share/" + package_name, ["package.xml"]),
     ],
-    install_requires=["setuptools"],
+    install_requires=requirements,
     zip_safe=True,
     maintainer="Carlos Fernando Baptista",
     maintainer_email="cfd.baptista@gmail.com",
-    description="A ROS2 pacakge for interfacing with the HC-SR04 ultrasonic sensor",
+    description="A ROS2 package for interfacing with the HC-SR04 ultrasonic sensor",
     license="MIT",
     tests_require=["pytest"],
     entry_points={
