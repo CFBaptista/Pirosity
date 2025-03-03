0725aec7952aea4637f65f13af342f9b43c77058
Carlos Fernando Baptista <cfd.baptista@gmail.com>
2025-03-04 00:02:58 +0100

    Update declare

--- pirosity/ros/src/hcsr04/hcsr04/ultrasonic_distance.py
+++ pirosity/ros/src/hcsr04/hcsr04/ultrasonic_distance.py
@@ -5,22 +5,25 @@ from rcl_interfaces.msg import ParameterDescriptor, ParameterType
 from rclpy.node import Node
 from std_msgs.msg import Float64
 
-from pirosity.core.sensors import UltrasonicSensorHCSR04, UltrasonicSensorHCSR04Data
+from pirosity.core.sensors import UltrasonicSensorHCSR04
 
 
 @dataclass(frozen=True)
 class UltrasonicDistancePublisherData:
     """
-    Data class to hold constants for the ultrasonic distance publisher.
+    Data class to hold defaults for the ultrasonic distance publisher.
 
     Attributes
     ----------
+    `speed_of_sound` : float
+        Speed of sound in meters per second.
     `queue_size` : int
         The size of the message queue.
     `timer` : float
         The period in seconds to publish the measured distance.
     """
 
+    speed_of_sound: float = 343.0
     queue_size: int = 10
     timer: float = 0.5
 
@@ -31,6 +34,8 @@ class UltrasonicDistancePublisher(Node):
     """
 
     def __init__(self) -> None:
+        super().__init__("ultrasonic_distance_publisher")
+
         self._declare_parameters()
         parameters = self._get_parameters()
 
@@ -40,7 +45,6 @@ class UltrasonicDistancePublisher(Node):
             speed_of_sound=parameters["speed_of_sound"],
         )
 
-        super().__init__("ultrasonic_distance_publisher")
         self.publisher_ = self.create_publisher(
             Float64, "ultrasonic_distance", parameters["queue_size"]
         )
@@ -49,21 +53,49 @@ class UltrasonicDistancePublisher(Node):
     def _declare_parameters(self) -> None:
         self.declare_parameter(
             "trigger_pin",
+            -1,
             ParameterDescriptor(
+                name="trigger_pin",
                 type=ParameterType.PARAMETER_INTEGER,
                 description="GPIO pin used to trigger the sensor.",
             ),
         )
         self.declare_parameter(
             "echo_pin",
+            -1,
             ParameterDescriptor(
+                name="echo_pin",
                 type=ParameterType.PARAMETER_INTEGER,
                 description="GPIO pin used to receive the echo signal.",
             ),
         )
-        self.declare_parameter("speed_of_sound", UltrasonicSensorHCSR04Data.speed_of_sound)
-        self.declare_parameter("queue_size", UltrasonicDistancePublisherData.queue_size)
-        self.declare_parameter("timer", UltrasonicDistancePublisherData.timer)
+        self.declare_parameter(
+            "speed_of_sound",
+            UltrasonicDistancePublisherData.speed_of_sound,
+            ParameterDescriptor(
+                name="speed_of_sound",
+                type=ParameterType.PARAMETER_DOUBLE,
+                description="Speed of sound in meters per second.",
+            ),
+        )
+        self.declare_parameter(
+            "queue_size",
+            UltrasonicDistancePublisherData.queue_size,
+            ParameterDescriptor(
+                name="queue_size",
+                type=ParameterType.PARAMETER_INTEGER,
+                description="The size of the message queue.",
+            ),
+        )
+        self.declare_parameter(
+            "timer",
+            UltrasonicDistancePublisherData.timer,
+            ParameterDescriptor(
+                name="timer",
+                type=ParameterType.PARAMETER_DOUBLE,
+                description="The period in seconds to publish the measured distance.",
+            ),
+        )
 
     def _get_parameters(self) -> dict:
         trigger_pin = self.get_parameter("trigger_pin").get_parameter_value().integer_value
@@ -72,13 +104,13 @@ class UltrasonicDistancePublisher(Node):
         queue_size = self.get_parameter("queue_size").get_parameter_value().integer_value
         timer = self.get_parameter("timer").get_parameter_value().double_value
 
-        parameters = dict(
-            trigger_pin=trigger_pin,
-            echo_pin=echo_pin,
-            speed_of_sound=speed_of_sound,
-            queue_size=queue_size,
-            timer=timer,
-        )
+        parameters = {
+            "trigger_pin": trigger_pin,
+            "echo_pin": echo_pin,
+            "speed_of_sound": speed_of_sound,
+            "queue_size": queue_size,
+            "timer": timer,
+        }
 
         return parameters
 
