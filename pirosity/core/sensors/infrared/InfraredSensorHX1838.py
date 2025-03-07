549388ecbda473435d263debf8e058c62130631a
Carlos Fernando Baptista <cfd.baptista@gmail.com>
2025-03-07 16:02:32 +0100

    Add UART read logic

--- pirosity/core/sensors/infrared/InfraredSensorHX1838.py
+++ pirosity/core/sensors/infrared/InfraredSensorHX1838.py
@@ -1,2 +1,23 @@
+from dataclasses import dataclass
+
+from gpiozero import Serial
+
+
+@dataclass
+class InfraredSensorHX1838Data:
+    name: str = "HX1838"
+
+
 class InfraredSensorHX1838:
-    pass
+    def __init__(self, receive_pin: int, baudrate=9600):
+        self._sensor = Serial(rx_pin=receive_pin, baudrate=baudrate)
+        self._baudrate = baudrate
+
+    def read_value(self):
+        while True:
+            value = self._sensor.read()
+            print(value)
+
+    @property
+    def baudrate(self) -> int:
+        return self._baudrate
