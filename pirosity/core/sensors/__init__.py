ba87f0fcb09b3bcc37f4fec5df37b1aa6feaecbe
Carlos Fernando Baptista <cfd.baptista@gmail.com>
2025-03-07 14:57:43 +0100

    Rename distance directory to ultrasonic

--- pirosity/core/sensors/__init__.py
+++ pirosity/core/sensors/__init__.py
@@ -1,4 +1,4 @@
-from .distance.UltrasonicSensorHCSR04 import UltrasonicSensorHCSR04, UltrasonicSensorHCSR04Data
 from .infrared.InfraredSensorHX1838 import InfraredSensorHX1838
+from .ultrasonic.UltrasonicSensorHCSR04 import UltrasonicSensorHCSR04, UltrasonicSensorHCSR04Data
 
 __all__ = ["InfraredSensorHX1838", "UltrasonicSensorHCSR04", "UltrasonicSensorHCSR04Data"]
